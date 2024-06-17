cc1a9c42ccc91e70b99f7c893d2a557e6a46a30a
Daniel Dalessandro Sousa Meneses <dg189948@gmail.com>
2024-06-16 21:52:33 -0300

    Feat: creating migrations

---
+++ src/main/resources/db/migration/V2_create-table-coupon.sql
@@ -0,0 +1,8 @@
+CREATE TABLE coupon (
+    id UUID gen_random_uuid() PRIMARY KEY,
+    code varchar(100) NOT NULL,
+    discount INTEGER NOT NULL,
+    valid TIMESTAMP NOT NULL,
+    event_id UUID,
+    FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
+);
